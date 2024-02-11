e8609854acf47f9669c75560a288eaa4292825b1
Barkamol <kamolbek0990@gmail.com>
2024-02-11 15:29:49 +0500

    new1

--- cpp/dynamic/new1.cpp
+++ cpp/dynamic/new1.cpp
@@ -2,6 +2,14 @@
 using namespace std;
 int main()
 {
+    int a = 4;
+
+    int *b = new int;
+    *b = 10;
+
+    cout << a << endl;
+    cout << b << endl;
+    cout << *b << endl;
 
     return 0;
 }
