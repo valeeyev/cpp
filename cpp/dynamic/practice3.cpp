d13c613230cc049cf31c82ceb609c8bcb9d5ecce
Barkamol <kamolbek0990@gmail.com>
2024-02-14 12:45:07 +0500

    added

---
+++ cpp/dynamic/practice3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void swap()
+
+    int main()
+{
+
+    int a, b;
+    cin >> a >> b;
+
+    swap(&a, &b);
+
+    return 0;
+}
--- cpp/malloc/pointer.cpp
+++ cpp/malloc/pointer.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 using namespace std;
-int main(){
-int n = 29;
-cout << &n << endl;
+int main()
+{
+    int a;
+    cin >> a;
+    int *n = new int(a);
 
-
-
-
-return 0;
+    cout << "pointer itself " << n << endl;
+    cout << "pointer address " << &n << endl;
+    cout << "variable address " << &a << endl;
+    cout << "value inside  " << *n << endl;
+    delete n;
+    return 0;
 }
