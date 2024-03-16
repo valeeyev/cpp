01450a07893ce83b7f4de4c8b9df11342c908d6c
Barkamol <kamolbek0990@gmail.com>
2024-03-16 12:00:08 +0500

    linked list updated

--- cpp/data_structures/linked.cpp
+++ cpp/data_structures/linked.cpp
@@ -1,17 +1,37 @@
 #include <iostream>
-#include <cstdlib>
-
+#include <stdlib.h>
 using namespace std;
 
 struct node
 {
-    int age;
-    node *ptr;
+    int list_number;
+    node *next;
 };
-// mening comentim
 
 int main(int argc, char *argv[])
 {
+    node *list = nullptr;
+
+    for (int i = 1; i < argc; i++)
+    {
+        // get values from comand line to the number field of node
+        int number = atoi(argv[i]); // now the input data is int type
+        node *n = new node;         // create temporary pointer to pop linked list
+        if (n == nullptr)
+        {
+            // freeing
+            return 1;
+        }
+        n->list_number = number;
+        n->next = list;
+        list = n;
+    }
+    node *p = list;
+    while (list != nullptr)
+    {
+        cout << p->list_number;
+        p = p->next;
+    }
 
     return 0;
 }
